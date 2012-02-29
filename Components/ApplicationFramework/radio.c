96ec4fdfef3504381e2800d77738cfe42c188139
zyfa2008@gmail.com <zyfa2008@gmail.com@a5f205dc-9840-07da-d807-790af1f6a724>
2012-02-29 15:08:24 +0000

    V1.1 release. three nodes works well now. and very stable, no duplicate frame is received. To-Do: 1. improve power to send more long distance. default seems is 0dbm

--- Components/ApplicationFramework/radio.c
+++ Components/ApplicationFramework/radio.c
@@ -1,60 +1,34 @@
 #include "framework.h"
+#include "nwk.h"
 
 /* the transmit and receive buffers */
 uint8_t rx[MAX_APP_PAYLOAD], tx[MAX_APP_PAYLOAD];
-linkID_t	receiveLinkID = 0;
-linkID_t	sendLinkID = 0;
 
-struct AppFrame* receive()
+struct AppFrame* receive(void)
 { 
-	uint16 		i = 0;
-	uint8 		len;
-	uint16 		maxTimes = 1000;
-	if (SMPL_SUCCESS != SMPL_LinkListen(&receiveLinkID))
+	uint8 len;
+	if( SMPL_Receive( SMPL_LINKID_LINEAR, rx, &len ) != SMPL_SUCCESS)
 	{
-		/* blink LED until we link successfully */
-		BSP_TOGGLE_LED1( );
 		return NULL;
 	}
-	/* sure we have received data */
-	//MRFI_DelayMs( 100 );
-	while( SMPL_Receive( receiveLinkID, rx, &len ) != SMPL_SUCCESS)
+	else
 	{
-		if(i >= maxTimes)
-		{
-			//return NULL;
-		}
-		//MRFI_DelayMs(10);
-		//BSP_TOGGLE_LED2( );
-		i++;
+		BSP_TOGGLE_LED2();
+		return (struct AppFrame*)rx;
 	}
-	
-	return (struct AppFrame*)rx;
 }
 
-smplStatus_t send(void *data, uint8 len)
+smplStatus_t send(struct AppFrame* data, uint8 len)
 {
-	/*
-	smplStatus_t rc;
-	struct AppFrame *frame = (struct AppFrame*)data;
-	uint8 nextAddr;
-	nextAddr = getNextAddress(frame->originalAddr, frame->finnalDstAddr, 1);
+	smplStatus_t 	rc;
+	uint8 				nextAddr;
+	nextAddr = getNextAddress(data->originalAddr, data->finnalDstAddr, 1);
 	if(nextAddr)
 	{
-		frame->dstAddr = nextAddr;
-		while(SMPL_SUCCESS != SMPL_Link(&sendLinkID, nextAddr))
-		{
-			sprintf(logTemp, "link to %d failed\n", frame->dstAddr);
-			log(ERROR, logTemp);
-			BSP_TOGGLE_LED2( );
-			//return SMPL_NO_LINK;
-		}
-		log(ERROR, "Link Success\n");
-		rc = SMPL_Send(sendLinkID, data, len);
-		MRFI_DelayMs( 5 );
-		SMPL_Unlink(sendLinkID);
+		data->dstAddr = nextAddr;
+		rc = SMPL_Send_Linear(data->dstAddr, (uint8_t*)data, len);
+		BSP_TOGGLE_LED1();
 		return rc;
 	}
-	*/
 	return 	SMPL_NO_LINK;
 }
