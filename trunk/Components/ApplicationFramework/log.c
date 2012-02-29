c6fa5547116ac2e7e1ef315714d7c583b5646700
zyfa2008@gmail.com <zyfa2008@gmail.com@a5f205dc-9840-07da-d807-790af1f6a724>
2012-02-29 13:29:02 +0000

    change folder name to ApplicationFramework

---
+++ trunk/Components/ApplicationFramework/log.c
@@ -0,0 +1,69 @@
+/*************************************
+ * log module
+ *
+ * Create date: 2011/11/22 
+ * Author: yongfa.zhou, email: zyfa2008@163.com
+ * bupt fop laboratory
+ */
+
+#include "framework.h"
+
+uint8 logBuffer[LOGBUFFERLEN];
+volatile uint16 logIndex = 0;
+char logTemp[LOGTEMP_SIZE];
+
+uint8 logLevel = INFO; 
+
+/* not implement, send to uart or lcd
+ */
+void log(uint8 logType, char* logContent)
+{
+	uint8 myAddr;
+	if(logType <= logLevel)
+	{
+		if(logIndex + strlen(logContent) + 1 > LOGBUFFERLEN)
+		{
+			removeOldLog(strlen(logContent) + 1);
+		}
+		/* olgIndex has updated by removeOldLog() function if has call it */
+		memcpy(logBuffer + logIndex, logContent, strlen(logContent) + 1);
+		logIndex += (strlen(logContent) + 1);
+		
+		myAddr = getMyAddress();
+		if(myAddr == APADDRESS)
+		{
+			console(logContent);
+		}
+	}
+}
+
+/* remove the oldest log, and append new log to the last
+ */
+void removeOldLog(uint16 newLogLength)
+{
+	uint16 i;
+	uint16 newIndex;
+	
+	i = newLogLength - 1;
+	/* search to the end log content */
+	while(logBuffer[i] != '\0' && i < LOGBUFFERLEN)
+	{
+		i++;
+	}
+	/* remove all log data */
+	if(i == LOGBUFFERLEN)
+	{
+		logIndex = 0;
+		return;
+	}
+	i++; /* point to new log content */
+	newIndex = i;
+	
+	for(; i<LOGBUFFERLEN; i++)
+	{
+		logBuffer[i - newIndex] = logBuffer[i];
+	}
+	logIndex -= newIndex;
+	return;
+}
+
